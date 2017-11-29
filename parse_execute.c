b8c6b7f06db604f24f03ae50873dbd2a53237da4
nikitaBor <nborisov@stuy.edu>
2017-11-29 16:37:28 -0500

    added function headers

--- parse_execute.c
+++ parse_execute.c
@@ -5,6 +5,11 @@
 #include <sys/wait.h>
 #include "redirect.h"
 
+//parse_args: takes in char * of line to parse and returns a char** populated with arguments that were seperated by spaces
+//uses strsep to seperate string by spaces
+/*preconditions:
+    *line must end with a NULL or newline
+*/
 char ** parse_args( char * line ) {
   char ** a = (char **)calloc(25, sizeof(char[256]));
   int index = 0;
@@ -29,6 +34,13 @@ char ** parse_args( char * line ) {
   return a;
 }
 
+
+//get_and_execute: has no arguments and doesn't return anything
+//function gets user inputed command w/ fgets, parses it, and executes it by forking off a child process
+/*preconditions: 
+    *user command can have multiple spaces between commands and flags/keywords
+    *using & token can cause bug if program opened with & is closed(will cause program to not wait for any processes to finish)
+*/
 void get_and_execute() {
   char *line;
   line = (char *) malloc(256 * sizeof(char));
